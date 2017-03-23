3f432abd9e3ea68a8b73e9a06a0faf4f6abe2e47
NiceStars <NiceStars@qq.com>
2017-03-23 15:33:39 +0800

    first commit[C

---
+++ app/src/main/java/com/butterknife/ContentActivity.java
@@ -0,0 +1,33 @@
+package com.butterknife;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import views.MyZoomImageView;
+
+/**
+ * Description :
+ * Created by NiceStars on 2017/3/23.
+ * Email:NiceStars.cn@gmail.com
+ */
+
+public class ContentActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.content_iv)
+    MyZoomImageView contentIv;
+    private int[] res = new int[]{R.mipmap.recycleview,R.mipmap.cardview,R.mipmap.butterknife};
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contentactivity);
+        ButterKnife.bind(this);
+
+        int p = getIntent().getIntExtra("position",-1);
+        contentIv.setImageResource(res[p]);
+    }
+}
---
+++ app/src/main/java/com/butterknife/MainActivity.java
@@ -0,0 +1,53 @@
+package com.butterknife;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+
+import adapter.CardViewAdapter;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.recycleview)
+    RecyclerView mainRecycle;
+    @BindView(R.id.activity_main)
+    LinearLayout activityMain;
+
+    private CardViewAdapter cardViewAdapter;
+
+    private String[] title = new String[]{"RecycleView的使用", "Cardview的使用", "控件注解框架的使用", "天气预报小工具"};
+    private String[] message = new String[]{"", "", "", ""};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        cardViewAdapter = new CardViewAdapter(MainActivity.this, title, message);
+        mainRecycle.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        mainRecycle.setAdapter(cardViewAdapter);
+
+
+    }
+
+    //CardView 的初步用法
+    // compile 'com.android.support:cardview-v7:25.0.1'  添加依赖
+    //  这个属于一种布局模式  在这个里面可以添加自己需要的控件  比如textview  Button  等等
+    //比较特色的属性有两个  app:cardCornerRadius="10dp" CardView的圆角度数
+    // app:cardElevation="5dp" 深度  Android Material Design 设计模式
+
+    //ButterKnife 注解框架
+    // 要想使用快捷键 alt+insert 一键注解控件的话  分为两步
+    //第一步  Android Studio 下载插件 file—settings-plugins-browse repositories 查找butterknife插件  然后安装
+    //第二步 在app  build.gradle 里面添加 compile 'com.jakewharton:butterknife:8.5.1'  apt 'com.jakewharton:butterknife-compiler:8.5.1'
+    //然后 在头部 添加 apply plugin: 'com.neenbedankt.android-apt'
+    //最后在项目的 build.gradle 里面添加   classpath 'com.neenbedankt.gradle.plugins:android-apt:1.8'
+    //刷新一下  使用的时候一定要在页面OnCreate方法里面 绑定一下  代码： ButterKnife.bind(this);
+
+}
