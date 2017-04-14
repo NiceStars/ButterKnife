e8006f66dab3714dbdf8e423fc294a23764c02e8
NiceStars <NiceStars@qq.com>
2017-04-14 17:22:22 +0800

    增加了集成友盟推送SDK的步骤图

--- app/src/main/java/com/butterknife/MainActivity.java
+++ app/src/main/java/com/butterknife/MainActivity.java
@@ -1,12 +1,15 @@
 package com.butterknife;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuItem;
 
 import android.widget.LinearLayout;
@@ -32,8 +35,8 @@ public class MainActivity extends AppCompatActivity {
 
     private CardViewAdapter cardViewAdapter;
 
-    private String[] title = new String[]{"RecycleView的使用", "Cardview的使用", "控件注解框架的使用", "链接至手机浏览器","小米推送服务"};
-    private String[] message = new String[]{"RecycleView使用的基本步骤 get", "Cardview使用的基本步骤 get", "控件注解框架的使用", "如何链接至手机浏览器","小米推送服务使用具体步骤"};
+    private String[] title = new String[]{"RecycleView的使用", "Cardview的使用", "控件注解框架的使用", "链接至手机浏览器","友盟推送"};
+    private String[] message = new String[]{"RecycleView使用的基本步骤 get", "Cardview使用的基本步骤 get", "控件注解框架的使用", "如何链接至手机浏览器","友盟推送SDK集成步骤"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +85,57 @@ public class MainActivity extends AppCompatActivity {
     //最后在项目的 build.gradle 里面添加   classpath 'com.neenbedankt.gradle.plugins:android-apt:1.8'
     //刷新一下  使用的时候一定要在页面OnCreate方法里面 绑定一下  代码： ButterKnife.bind(this);
 
+
+
+    //集成友盟推送的步骤：
+    //1.下载友盟推送sdk 并当做库的模式导入到项目里面
+    //2.在友盟后台注册app  并拿到UMENG_APPKEY和UMENG_MESSAGE_SECRET 在清单文件里面配置一下
+    /*<meta-data
+    android:name="UMENG_APPKEY"
+    android:value="58eed133677baa67110021fa"></meta-data>
+    <meta-data
+    android:name="UMENG_MESSAGE_SECRET"
+    android:value="f758dd80530362ed5a6a5b8cedb1e11a"></meta-data>*/
+    //3.在application里面注册一下 umeng的信息
+    /*PushAgent mPushAgent = PushAgent.getInstance(this);
+    //注册推送服务，每次调用register方法都会回调该接口
+    mPushAgent.register(new IUmengRegisterCallback() {
+        @Override
+        public void onSuccess(String deviceToken) {
+            //注册成功会返回device token
+            Log.e("Application", deviceToken);
+        }
+        @Override
+        public void onFailure(String s, String s1) {
+            Log.e("Application", s);
+            Log.e("Application", s1);
+        }
+    });
+    UmengMessageHandler messageHandler = new UmengMessageHandler() {
+        @Override
+        public void dealWithCustomMessage(final Context context, final UMessage msg) {
+            new Handler(getMainLooper()).post(new Runnable() {
+
+                @Override
+                public void run() {
+                    // 对于自定义消息，PushSDK默认只统计送达。若开发者需要统计点击和忽略，则需手动调用统计方法。
+                    boolean isClickOrDismissed = true;
+                    if (isClickOrDismissed) {
+                        //自定义消息的点击统计
+                        UTrack.getInstance(getApplicationContext()).trackMsgClick(msg);
+                    } else {
+                        //自定义消息的忽略统计
+                        UTrack.getInstance(getApplicationContext()).trackMsgDismissed(msg);
+                    }
+                    Toast.makeText(context, msg.custom, Toast.LENGTH_LONG).show();
+                  //这里加入提醒功能 比如notification
+                }
+            });
+        }
+    };
+    mPushAgent.setMessageHandler(messageHandler);*/
+    //4.在首页Activity里面加上  PushAgent.getInstance(this).onAppStart();
+    //5.集成功完成
 
 
 }
